0c7a5aab71b3a9ceffa465898b1d8c108937ec56
G-Y-Chan <changuanyu2904@gmail.com>
2024-11-05 19:01:10 +0800

    Create component that will show preview of resume

---
+++ src/components/Resume.jsx
@@ -0,0 +1,14 @@
+function Resume(props) {
+    return (
+        <div
+            style={{
+                backgroundColor:"white",
+                width:"40vw",
+                height:"100vh"
+            }}
+        >
+        </div>
+    )
+}
+
+export default Resume;
